f09ec8622c5725c1074fbf0fd1e14c27a8eaaefa
takaosecond <takaosecond@icloud.com>
2022-10-10 09:18:37 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState([]);
+  const ref = useRef();
+
+  const handleSearch = () => {
+    console.log(ref.current.value);
+
+    // フィルタリング機能
+    setSearchQuery(
+      users.filter((user) =>
+        user.name.toLowerCase().includes(ref.current.value)
+      )
+    );
+  };
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="main">
+        <h2>検索アプリ</h2>
+        <input type="text" ref={ref} onChange={() => handleSearch()} />
+        <div className="content">
+          {searchQuery.map((user) => (
+            <div className="box" key={user.id}>
+              <h3>{user.name}</h3>
+              <hr />
+              <p>{user.email}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
